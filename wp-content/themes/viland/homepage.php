18116de9d579c2fe413be6b8ed50250c214ba9d2
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2017-02-22 18:18:20 +0700

    Style tralvel block of homepage

--- wp-content/themes/viland/homepage.php
+++ wp-content/themes/viland/homepage.php
@@ -190,59 +190,62 @@ get_header(); ?>
     </div>
 
     <div class="travel-info">
-        <div class="menu-table-travel">
-            <ul>
-                <li class="active"><a href="#blog" data-toggle="tab">Travel Blog</a></li>
-                <li><a href="#videos" data-toggle="tab">Travel videos</a></li>
-                <li><a href="#guides" data-toggle="tab">Travel guides</a></li>
-            </ul>
-        </div>
-        <div class="tab-content">
-            <div class="tab-pane fade active in" id="blog">
-                <?php
-                $args = array(
-                    'post_type'=> 'post',
-                    'orderby' => 'post_date',
-                    'order'    => 'DESC'
-                );
+        <div class="container">
+            <div class="menu-tab-travel">
+                <ul>
+                    <li class="active"><a href="#blog" data-toggle="tab">Travel Blog</a></li>
+                    <li><a href="#videos" data-toggle="tab">Travel videos</a></li>
+                    <li><a href="#guides" data-toggle="tab">Travel guides</a></li>
+                </ul>
+            </div>
+            <div class="tab-content">
+                <div class="tab-pane active" id="blog">
+                    <?php
+                    $args = array(
+                        'post_type'=> 'post',
+                        'orderby' => 'post_date',
+                        'order'    => 'DESC'
+                    );
 
-                $the_query = new WP_Query( $args );
-                echo '<ul class="recent-blog">';
-                if($the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();?>
-                    <li class="item-blog">
-                        <a href="<?php echo get_permalink(get_the_ID())?>">
-                            <?php echo get_the_post_thumbnail(get_the_ID(), array(800,600));?>
-                            <p><?php the_title(); ?></p>
-                            <p class="read-more">Read more</p>
-                        </a>
-                    </li>
-                <?php endwhile; else: ?>
-                <?php endif; wp_reset_postdata(); ?>
-            </div>
-            <div class="tab-pane fade active in" id="videos">
-                <h2>Content of videos</h2>
-            </div>
-            <div class="tab-pane fade active in" id="guides">
-                <?php
-                $args = array(
-                    'post_type'=> 'dt_places',
-                    'orderby' => 'post_date',
-                    'order'    => 'DESC'
-                );
+                    $the_query = new WP_Query( $args );
+                    echo '<ul class="recent-blog">';
+                    if($the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();?>
+                        <li class="item-blog">
+                            <a href="<?php echo get_permalink(get_the_ID())?>">
+                                <?php echo get_the_post_thumbnail(get_the_ID(), array(800,600));?>
+                                <p><?php the_title(); ?></p>
+                                <p class="read-more">Read more</p>
+                            </a>
+                        </li>
+                    <?php endwhile; else: ?>
+                    <?php endif; wp_reset_postdata(); ?>
+                </div>
+                <div class="tab-pane" id="videos">
+                    <h2>Content of videos</h2>
+                </div>
+                <div class="tab-pane" id="guides">
+                    <?php
+                    $args = array(
+                        'post_type'=> 'dt_places',
+                        'orderby' => 'post_date',
+                        'order'    => 'DESC'
+                    );
 
-                $the_query = new WP_Query( $args );
-                echo '<ul class="recent-guides">';
-                if($the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();?>
-                    <li class="guide">
-                        <a href="<?php echo get_permalink(get_the_ID())?>">
-                            <?php echo get_the_post_thumbnail(get_the_ID(), array(800,600));?>
-                            <p><?php the_title(); ?></p>
-                            <p class="read-more">Read more</p>
-                        </a>
-                    </li>
-                <?php endwhile; else: ?>
-                <?php endif; wp_reset_postdata(); ?>
+                    $the_query = new WP_Query( $args );
+                    echo '<ul class="recent-guides">';
+                    if($the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();?>
+                        <li class="guide">
+                            <a href="<?php echo get_permalink(get_the_ID())?>">
+                                <?php echo get_the_post_thumbnail(get_the_ID(), array(800,600));?>
+                                <p><?php the_title(); ?></p>
+                                <p class="read-more">Read more</p>
+                            </a>
+                        </li>
+                    <?php endwhile; else: ?>
+                    <?php endif; wp_reset_postdata(); ?>
+                </div>
             </div>
+            <a class="button view-all-travel" href="#">View All</a>
         </div>
     </div>
 </div>
